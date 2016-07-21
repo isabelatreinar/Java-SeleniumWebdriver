3ed89bdeb1dee85c57c6662ad5f00a91965d6336
isabela_oliveira <isabela_oliveira@905d82e2-5594-4323-9dd4-6f5f8ef63280>
2016-07-21 18:36:44 +0000

    Criação de classe Login

--- trunk/TesteAutomatizado/src/br/com/marph/geicom/funcionalidade/tipoBase/TipoBase.java
+++ trunk/TesteAutomatizado/src/br/com/marph/geicom/funcionalidade/tipoBase/TipoBase.java
@@ -1,6 +1,37 @@
 package br.com.marph.geicom.funcionalidade.tipoBase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import br.com.marph.geicom.util.LoginSistema;
 
 public class TipoBase {
+
+	private WebDriver driver;
+
+	@Before
+		public void startUp(){
+		driver = new FirefoxDriver();
+	}
 	
+	/*@After
+	public void driverClose(){
+		driver.quit();
+	}*/
 	
+	@Test
+	public void browser(){
+		LoginSistema.acessarUrl(driver);
+		
+		LoginSistema.acessarSistema(driver);
+	}
+		
+
 }
+
+//LoginSistema.acessarSistema(driver);
+		/*WebElement botaoEnviar = driver.findElement(By.id("btnAcessar"));
+		    botaoEnviar.click();*/
