34aa3edae64c8c01dca4e87c992f28667e2f6a57
gleidson <gleidson@gleidson-THINK>
2016-07-22 15:06:26 -0300

    organizando imports

--- src/test/java/br/com/marph/geicom/login/TesteLoginComplexo.java
+++ src/test/java/br/com/marph/geicom/login/TesteLoginComplexo.java
@@ -5,10 +5,8 @@ import java.util.List;
 import java.util.Map.Entry;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
